cbbd5826539bc8ec58ef11c28786ae8fa83d073b
Chris Woodle <chris@droplit.io>
2018-09-09 08:18:49 -0400

    timing patch

--- dab/embedded/index.js
+++ dab/embedded/index.js
@@ -6,26 +6,30 @@ var port = new SerialPort('/dev/ttyACM0', {
 const termChar = '\n';
 
 let packet = '';
+let queue = null;
 
 port.on('data', function (data) {
     // console.log('Data:', data.toString());
     packet += data.toString();
     if (packet.includes(termChar)) {
         console.log(packet);
+        if (!queue) queue = packet;
         packet = '';
+
     }
 });
 
 const redis = require("redis");
 const client = redis.createClient({
-        "host": "35.229.77.174",
-        "password": "myUnguessablePazzzzzword321"
-    });
+    "host": "35.229.77.174",
+    "password": "myUnguessablePazzzzzword321"
+});
 
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
 setInterval(() => {
-    client.publish('dab', packet);
+    client.publish('dab', queue);
+    queue = null;
 }, 1000);
